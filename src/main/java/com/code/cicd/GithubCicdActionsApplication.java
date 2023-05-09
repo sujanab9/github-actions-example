c702d578e27bbb04d38c30de97827d5b09ed3089
sujanab9 <sujanab9@gmail.com>
2023-05-09 14:57:58 -0400

    code

---
+++ src/main/java/com/code/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,30 @@
+package com.code.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Newjersey";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+	
+	
+	/* echo "# github-actions-example" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/sujanab9/github-actions-example.git
+	git push -u origin main
+*/
+}
